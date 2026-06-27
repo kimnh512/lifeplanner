88edcccd7db98b910c2d96caf6fca5fb5ec41698
kimnh512 <kimnahyun71day@gmail.com>
2026-06-27 22:06:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lifeplanner-v2';
+const CACHE = 'lifeplanner-v3';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
